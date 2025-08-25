c56faf09bad50e7bc71f9df1d3efe6f19d5e3c2e
Daniils Firgers <dfirger@gmail.com>
2025-08-25 22:27:41 +0300

    feat: create GetMarketHours

--- broker/internal/market/quant.go
+++ broker/internal/market/quant.go
@@ -1,1 +1,16 @@
 package market
+
+import (
+	"fmt"
+	"time"
+)
+
+func GetMarketHours() (start, end time.Time, err error) {
+	if time.Now().Weekday() == time.Saturday || time.Now().Weekday() == time.Sunday {
+		return time.Time{}, time.Time{}, fmt.Errorf("market is closed on weekends")
+	}
+	open := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 13, 30, 0, 0, time.UTC)
+	close := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 20, 0, 0, 0, time.UTC)
+	return open, close, nil
+
+}
