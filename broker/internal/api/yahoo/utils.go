5f17a8dcbe81e86176ce8643775bcd64d5ab618d
Daniils Firgers <dfirger@gmail.com>
2025-08-25 22:27:29 +0300

    feat:  move getWindowMaxDuration and getRequestPeriods to utils

---
+++ broker/internal/api/yahoo/utils.go
@@ -0,0 +1,34 @@
+package yahoo
+
+import (
+	"broker/internal/market"
+	"fmt"
+	"time"
+)
+
+func getWindowMaxDuration(start, end time.Time, requestedMaxPeriod time.Duration) time.Duration {
+	duration := end.Sub(start)
+	if duration < requestedMaxPeriod {
+		return duration
+	}
+	return requestedMaxPeriod
+}
+
+func getRequestPeriods(periodEnd time.Time, windowLength time.Duration) (from, to time.Time, err error) {
+	open, close, error := market.GetMarketHours()
+	if error != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("failed to get market hours: %w", error)
+	}
+
+	if periodEnd.After(close) {
+		return time.Time{}, time.Time{}, fmt.Errorf("period end %s is after market close %s", periodEnd, close)
+	}
+
+	start := periodEnd.Add(-windowLength)
+
+	if start.Before(open) {
+		start = open
+	}
+
+	return start, periodEnd, nil
+}
