e576e435c7a0b809e43cab436f4c0dd2ed731867
Daniils Firgers <dfirger@gmail.com>
2025-08-25 22:27:08 +0300

    feat: now can request timeseries with end and start params

--- broker/internal/api/yahoo/parser.go
+++ broker/internal/api/yahoo/parser.go
@@ -1,15 +1,16 @@
 package yahoo
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"time"
 )
 
-const MINIMUM_WINDOW = 5 * time.Minute
+const MINIMUM_DURATION = 3 * time.Minute
 const DEFAULT_WINDOW_COUNT = 5
 
-func checkPriceVolumeTrend(data YahooSymbolOCHL, maxWindow time.Duration, windowsCount int) {
+func checkPriceVolumeTrend(data YahooSymbolOCHL, maxDuration time.Duration) {
 	if data.Chart.Result == nil || len(data.Chart.Result) == 0 {
 		return
 	}
@@ -28,9 +29,21 @@ func checkPriceVolumeTrend(data YahooSymbolOCHL, maxWindow time.Duration, window
 		return
 	}
 
-	cutoff := time.Now().Truncate(time.Minute).Add(-maxWindow)
+	lastTimeSec := result.Timestamp[len(result.Timestamp)-1]
+	if lastTimeSec == nil {
+		log.Printf("No valid timestamp found for symbol %s", *result.Meta.Symbol)
+		return
+	}
 
-	var points []DinanceTsPoint
+	lastTime := time.Unix(*lastTimeSec, 0).Truncate(time.Minute).UTC()
+	lastPrice := quote.Close[len(quote.Close)-1]
+	lastVolume := quote.Volume[len(quote.Volume)-1]
+
+	windows := deriveWindowSteps(maxDuration, MINIMUM_DURATION, DEFAULT_WINDOW_COUNT, lastTime)
+
+	var targets []DinanceTrendTsPoint
+	cumulativeVolume := int64(0)
+
 	// Timestamps are sorted in from oldest to newest, so we can iterate through them
 	for i, ts := range result.Timestamp {
 		if ts == nil {
@@ -38,9 +51,6 @@ func checkPriceVolumeTrend(data YahooSymbolOCHL, maxWindow time.Duration, window
 		}
 
 		timestamp := time.Unix(*ts, 0).Truncate(time.Minute).UTC()
-		if timestamp.Before(cutoff) {
-			continue
-		}
 
 		if quote.Close[i] == nil || quote.Volume[i] == nil {
 			continue
@@ -51,59 +61,36 @@ func checkPriceVolumeTrend(data YahooSymbolOCHL, maxWindow time.Duration, window
 			continue
 		}
 
-		points = append(points, DinanceTsPoint{
-			Time:   timestamp,
-			Close:  *quote.Close[i],
-			Volume: *quote.Volume[i],
-		})
-	}
+		cumulativeVolume += *quote.Volume[i]
 
-	if len(points) == 0 {
-		log.Printf("No valid points found for symbol %s", *result.Meta.Symbol)
-		return
-	}
-	computeWindowTrends(points, maxWindow)
-}
-
-func computeWindowTrends(points []DinanceTsPoint, maxWindow time.Duration) {
-	latest := points[len(points)-1]
-
-	windows := deriveWindowSteps(maxWindow, MINIMUM_WINDOW, DEFAULT_WINDOW_COUNT, latest.Time)
-	for w, _ := range windows {
-		log.Printf("Window: %s", w.Format(time.RFC3339))
-	}
-	var targets []DinanceTrendTsPoint
-	cumulativeVolume := int64(0)
-
-	for _, p := range points {
-		cumulativeVolume += p.Volume
-
-		if _, exists := windows[p.Time]; exists {
+		if _, exists := windows[timestamp]; exists {
 			targets = append(targets, DinanceTrendTsPoint{
 				DinanceTsPoint: DinanceTsPoint{
-					Time:   p.Time,
-					Close:  p.Close,
-					Volume: p.Volume,
+					Time:   timestamp,
+					Close:  *quote.Close[i],
+					Volume: *quote.Volume[i],
 				},
 				CumulativeVolume: cumulativeVolume,
 			})
 
-			delete(windows, p.Time)
-			break
+			delete(windows, timestamp)
 		}
 	}
 
 	if len(targets) == 0 {
+		log.Printf("No valid targets formed for symbol %s", *result.Meta.Symbol)
 		return
 	}
+
 	for _, target := range targets {
-		priceChange := (latest.Close - target.Close) / target.Close * 100
-		avgVolume := float64(target.CumulativeVolume) / latest.Time.Sub(target.Time).Minutes()
-		volumeRatio := float64(latest.Volume) / avgVolume
+		priceChange := (*lastPrice - target.Close) / target.Close * 100
+		avgVolume := float64(target.CumulativeVolume) / lastTime.Sub(target.Time).Minutes()
+		fmt.Printf("VOlume: %d, Avg Volume: %.2f, Last Volume: %d\n", target.CumulativeVolume, avgVolume, *lastVolume)
+		volumeRatio := float64(*lastVolume) / avgVolume
 		log.Printf("Window %s: Price change: %.2f%%, Volume ratio: %.2f", target.Time.Format(time.RFC3339), priceChange, volumeRatio)
 	}
-}
 
+}
 func deriveWindowSteps(maxDuration, minDuration time.Duration, count int, targetTime time.Time) map[time.Time]struct{} {
 	if count <= 0 {
 		return nil
@@ -124,7 +111,7 @@ func deriveWindowSteps(maxDuration, minDuration time.Duration, count int, target
 			windowSteps[stepTime] = struct{}{}
 		}
 
-		currDuration = time.Duration(float64(currDuration) * step)
+		currDuration = time.Duration(float64(currDuration) * step).Truncate(time.Minute)
 	}
 	return windowSteps
 }
