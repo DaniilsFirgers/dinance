c615686e77e399a2cfc58ceb2e56cd16fd84f03f
Daniils Firgers <dfirger@gmail.com>
2025-08-25 22:26:36 +0300

    feat: continue working on quote client

--- broker/internal/api/yahoo/client.go
+++ broker/internal/api/yahoo/client.go
@@ -17,35 +17,46 @@ type YahooClient struct {
 }
 
 func (y YahooClient) Run(cron *cron.Cron) {
-	cron.AddFunc("yahoo-quote", "@every 10s", func() {
-		if err := y.GetQuotesData(time.Hour * 48); err != nil {
-			log.Println("Error fetching quotes data:", err)
-		}
-	})
+	if err := y.GetQuotesData(time.Hour * 6); err != nil {
+		log.Println("Error fetching quotes data:", err)
+	}
+	// cron.AddFunc("yahoo-quote", "@every 10s", func() {
+	// 	if err := y.GetQuotesData(time.Hour * 3); err != nil {
+	// 		log.Println("Error fetching quotes data:", err)
+	// 	}
+	// })
 }
 
-func (y YahooClient) GetQuotesData(cutOffTime time.Duration) error {
+func (y YahooClient) GetQuotesData(maxDuration time.Duration) error {
 	var wg sync.WaitGroup
+
+	periodStart := time.Now().UTC().Truncate(time.Minute)
+	start, end, err := getRequestPeriods(periodStart, maxDuration)
+	if err != nil {
+		return fmt.Errorf("failed to get request periods: %w", err)
+	}
+	windowMaxDuration := getWindowMaxDuration(start, end, maxDuration)
 
 	for _, symbol := range y.TickersConfig.Tickers {
 		wg.Add(1)
 		go func(sym string) {
 			defer wg.Done()
-			data, err := y.getQuoteData(sym)
+			data, err := y.GetQuoteData(sym, start, end)
 			if err != nil {
 				log.Printf("Error fetching quote for %s: %v\n", sym, err)
 				return
 			}
-			checkPriceVolumeTrend(data, cutOffTime, DEFAULT_WINDOW_COUNT)
+			checkPriceVolumeTrend(data, windowMaxDuration)
 		}(symbol)
 	}
 	wg.Wait()
 	return nil
 }
 
-func (y YahooClient) getQuoteData(symbol string) (YahooSymbolOCHL, error) {
+func (y YahooClient) GetQuoteData(symbol string, startTime, endTime time.Time) (YahooSymbolOCHL, error) {
 	headers := httpclient.GetHeaders("https://finance.yahoo.com/")
-	url := fmt.Sprintf("https://query1.finance.yahoo.com/v8/finance/chart/%s?interval=1m&range=1d", symbol)
+	fmt.Printf("Fetching data for symbol: %s from %s to %s\n", symbol, startTime.Unix(), endTime.Unix())
+	url := fmt.Sprintf("https://query1.finance.yahoo.com/v8/finance/chart/%s?interval=1m&range=1d&period1=%d&period2=%d", symbol, startTime.Unix(), endTime.Unix())
 
 	res, err := httpclient.Get(url, headers)
 	if err != nil {
