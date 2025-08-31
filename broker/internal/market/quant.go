fdab669b23e4a75ee1efc625bcd13f37000c2254
Daniils Firgers <dfirger@gmail.com>
2025-08-31 22:16:01 +0300

    feat: create Hours map and use it get correct market hours for different exchanges

--- broker/internal/market/quant.go
+++ broker/internal/market/quant.go
@@ -5,12 +5,47 @@ import (
 	"time"
 )
 
-func GetMarketHours() (start, end time.Time, err error) {
+const (
+	US Exchange = "us"
+	EU Exchange = "eu"
+)
+
+var marketHours = map[Exchange]MarketWorkingHours{
+	US: {
+		OpenHour:    13,
+		OpenMinute:  30,
+		CloseHour:   20,
+		CloseMinute: 0,
+		Timezone:    "America/New_York",
+	},
+	EU: {
+		OpenHour:    10,
+		OpenMinute:  0,
+		CloseHour:   18,
+		CloseMinute: 30,
+		Timezone:    "Europe/Riga",
+	},
+}
+
+func GetMarketHours(region Exchange) (start, end time.Time, err error) {
 	if time.Now().Weekday() == time.Saturday || time.Now().Weekday() == time.Sunday {
 		return time.Time{}, time.Time{}, fmt.Errorf("market is closed on weekends")
 	}
-	open := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 13, 30, 0, 0, time.UTC)
-	close := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 20, 0, 0, 0, time.UTC)
+
+	hours, exists := marketHours[region]
+	if !exists {
+		return time.Time{}, time.Time{}, fmt.Errorf("unknown market region: %s", region)
+	}
+
+	loc, err := time.LoadLocation(hours.Timezone)
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("failed to load timezone %s: %v", hours.Timezone, err)
+	}
+
+	tNow := time.Now().In(loc)
+
+	open := time.Date(tNow.Year(), tNow.Month(), tNow.Day(), hours.OpenHour, hours.OpenMinute, 0, 0, loc)
+	close := time.Date(tNow.Year(), tNow.Month(), tNow.Day(), hours.CloseHour, hours.CloseMinute, 0, 0, loc)
 	return open, close, nil
 
 }
